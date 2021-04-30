1bf10087657f6125d541d5aed7909ef0abea972d
Ted Keim <tedkeim@yahoo.com>
2021-04-30 13:19:58 -0400

    changed class and id in html on schedule, added save button

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
 $("#currentDay").text(moment().format("Do, MMMM, YYYY, h:mm a"));
 
 
-var past = document.querySelector("#past");
-var present = document.querySelector("#present");
-var future = document.querySelector("#future");
 
