354738b105ab3cb5cb98a92e88824212715f67fe
Ted Keim <tedkeim@yahoo.com>
2021-04-30 13:32:40 -0400

    started js, added variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
 $("#currentDay").text(moment().format("Do, MMMM, YYYY, h:mm a"));
 
-
-
+var text9 = $("#text9");
+var text10 = $("#text10");
+var text11 = $("#text11");
+var text12 = $("#text12");
+var text13 = $("#text13");
+var text14 = $("#text14");
+var text15 = $("#text15");
+var text16 = $("#text16");
+var text17 = $("#text17");
+var resetBtn = $('#clear');
+const textContent = document.getElementsByClassName('content');
+var saveBtn = $('.row').children('.btn');
