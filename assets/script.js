364b64a8afd27ffbd11f543b001278e43382be00
Ted Keim <tedkeim@yahoo.com>
2021-04-30 12:47:42 -0400

    added present time in header, added more to schedule format using html and css

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+$("#currentDay").text(moment().format("Do, MMMM, YYYY, h:mm a"));
+
+
+var past = document.querySelector("#past");
+var present = document.querySelector("#present");
+var future = document.querySelector("#future");
+
