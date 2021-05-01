8bdcedff75f21cf798b7fb9453ec15fdffa46810
Ted Keim <tedkeim@yahoo.com>
2021-05-01 08:13:31 -0400

    added function that saves event into local storage

--- assets/script.js
+++ assets/script.js
@@ -13,11 +13,36 @@ const textContent = document.getElementsByClassName('content');
 var saveBtn = $('.row').children('.btn');
 
 
+saveBtn.on("click", function(){
+    console.log('saved??')
+    alert('Your Event Has Been Saved')
+    
+    localStorage.setItem("9AM", (text1.val()))
+    localStorage.setItem("10AM", (text2.val()))
+    localStorage.setItem("11AM", (text3.val()))
+    localStorage.setItem("12PM", (text4.val()))
+    localStorage.setItem("1PM", (text5.val()))
+    localStorage.setItem("2PM", (text6.val()))
+    localStorage.setItem("3PM", (text7.val()))
+    localStorage.setItem("4PM", (text8.val()))
+    localStorage.setItem("5PM", (text9.val()))
+})
+//appends text boxs with information from local storage
+text1.append(localStorage.getItem("9AM"));
+text2.append(localStorage.getItem("10AM"));
+text3.append(localStorage.getItem("11AM"));
+text4.append(localStorage.getItem("12PM"));
+text5.append(localStorage.getItem("1PM"));
+text6.append(localStorage.getItem("2PM"));
+text7.append(localStorage.getItem("3PM"));
+text8.append(localStorage.getItem("4PM"));
+text9.append(localStorage.getItem("5PM"));
+
+
 const rows = document.getElementsByClassName("row");
-let currentHour = parseInt(moment().format('H'));
+let currentHour = parseInt(moment().format('h'));
 
-
-Array.from(rows).forEach(row=>{
+Array.from(rows).forEach(row =>{
     let
         rowIDString = row.id,
         rowHour;
@@ -27,9 +52,9 @@ Array.from(rows).forEach(row=>{
     if (rowHour){
         if (currentHour === rowHour){
             setColor(row, "red");
-        } else if ((currentHour < rowHour) && (currentHour >= rowHour-7)){
+        } else if ((currentHour < rowHour) && (currentHour >= rowHour-12)){
             setColor(row, "green");
-        }else{
+        } else{
         setColor(row, "lightgrey");
     }
 }
