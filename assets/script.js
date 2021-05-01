d1edb217e38946951b2a2752bffc70505a2b2776
Ted Keim <tedkeim@yahoo.com>
2021-05-01 07:46:54 -0400

    added function that changes row color according to the hour

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,14 @@
 $("#currentDay").text(moment().format("Do, MMMM, YYYY, h:mm a"));
 
+var text1 = $("#text1");
+var text2 = $("#text2");
+var text3 = $("#text3");
+var text4 = $("#text4");
+var text5 = $("#text5");
+var text6 = $("#text6");
+var text7 = $("#text7");
+var text8 = $("#text8");
 var text9 = $("#text9");
-var text10 = $("#text10");
-var text11 = $("#text11");
-var text12 = $("#text12");
-var text13 = $("#text13");
-var text14 = $("#text14");
-var text15 = $("#text15");
-var text16 = $("#text16");
-var text17 = $("#text17");
-var resetBtn = $('#clear');
 const textContent = document.getElementsByClassName('content');
 var saveBtn = $('.row').children('.btn');
 
@@ -17,4 +16,26 @@ var saveBtn = $('.row').children('.btn');
 const rows = document.getElementsByClassName("row");
 let currentHour = parseInt(moment().format('H'));
 
+
+Array.from(rows).forEach(row=>{
+    let
+        rowIDString = row.id,
+        rowHour;
+    if (rowIDString){
+        rowHour = parseInt(rowIDString);
+    }
+    if (rowHour){
+        if (currentHour === rowHour){
+            setColor(row, "red");
+        } else if ((currentHour < rowHour) && (currentHour >= rowHour-7)){
+            setColor(row, "green");
+        }else{
+        setColor(row, "lightgrey");
+    }
+}
+});
+//sets the color of the rows
+function setColor(element, color){
+    element.style.backgroundColor = color;
+}
 
